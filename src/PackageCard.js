27845d3799e42cb423867022597baa851f524abb
Jayden H <jaydenhutchinson747@gmail.com>
2025-06-02 13:42:08 -0700

    update to package cards

--- src/PackageCard.js
+++ src/PackageCard.js
@@ -1,8 +1,19 @@
-function PackageCard({ title, symbol }) {
+function PackageCard({ title, symbol, rewards, bonus }) {
   return (
     <div id={title} class="package">
       <img alt={title} src={symbol} />
       <h1>{title}</h1>
+      <div className="rewards">
+        <p>Rewards</p>
+        <ul>
+          {rewards.map((reward, index) => (
+            <li key={index} className="reward">
+              {reward}
+            </li>
+          ))}
+        </ul>
+        <p>{bonus}</p>
+      </div>
     </div>
   );
 }
--- src/Packages.js
+++ src/Packages.js
@@ -6,10 +6,30 @@ function Packages() {
     <section id="packages">
       <h1 class="section-heading">Packages</h1>
       <div class="content">
-        <PackageCard title="gold" symbol="/imgs/ranks/gold-rank.webp" />
-        <PackageCard title="platinum" symbol="/imgs/ranks/platinum-rank.webp" />
-        <PackageCard title="diamond" symbol="/imgs/ranks/diamond-rank.webp" />
-        <PackageCard title="master" symbol="/imgs/ranks/master-rank.webp" />
+        <PackageCard
+          title="gold"
+          symbol="/imgs/ranks/gold-rank.webp"
+          rewards={["Item 1", "Item 2", "Item 3"]}
+          bonus="Bonus Description"
+        />
+        <PackageCard
+          title="platinum"
+          symbol="/imgs/ranks/platinum-rank.webp"
+          rewards={["Item 1", "Item 2", "Item 3"]}
+          bonus="Bonus Description"
+        />
+        <PackageCard
+          title="diamond"
+          symbol="/imgs/ranks/diamond-rank.webp"
+          rewards={["Item 1", "Item 2", "Item 3"]}
+          bonus="Bonus Description"
+        />
+        <PackageCard
+          title="master"
+          symbol="/imgs/ranks/master-rank.webp"
+          rewards={["Item 1", "Item 2", "Item 3"]}
+          bonus="Bonus Description"
+        />
       </div>
       <h2>Add Ons</h2>
     </section>
